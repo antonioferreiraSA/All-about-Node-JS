244f696d58d01cf4596967beac968368a52bf355
antonioferreiraSA <ferreiratoni112@gmail.com>
2021-06-10 00:04:39 +0200

    done with section

---
+++ section1/HelloNode.js
@@ -0,0 +1,12 @@
+'use strict';
+const http = require ('http');
+
+
+http 
+
+.createServer((req, res) ={
+  res.writeHead(200, {'Content-type': 'text/html'});
+  res.end('<h1> HEllo NodeJS</h1>');
+})
+
+.listen(3000, () = > console.log('Server running on port 3000'));
---
+++ section2/BasicRouter.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const http = require('http');
+
+function  router (req, res) {
+ res.writeHead(200, {'Content-type': 'text/html'});
+ res.end('<h1>Hello Router</h1>');
+}
+
+http.createServer(router).listen(3000, () => {
+  console.log('server running on port 3000')
+})
